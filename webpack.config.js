a4e8f7d2cb94b8c691dd04a4ea73707ab13ef275
mat <mathiasbelleau@gmail.com>
2021-03-06 14:04:49 -0500

    init

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+var path = require('path');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const GitRevisionPlugin = require("git-revision-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+
+const gitRevisionPlugin = new GitRevisionPlugin();
+
+const devtool = () => {
+      return { devtool: "inline-source-map" };
+  
+    return {};
+  };
+
+const devServer = () => {
+      return {
+        devServer: {
+          contentBase: "./dist",
+          open: false,
+        },
+      };
+  
+    return {};
+  };
+
+module.exports = {
+  //...
+  ...devtool(),
+  ...devServer(),
+
+  entry: "./src/index.js",
+
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new HtmlWebpackPlugin({
+      title: "jsrlt",
+      template: "index.html",
+      version: gitRevisionPlugin.commithash().slice(0, 7),
+    }),
+  ],
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)\/(?!geotic)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [
+              "@babel/plugin-proposal-class-properties",
+              "@babel/plugin-proposal-private-methods",
+            ],
+          },
+        },
+      },
+    ],
+  },
+};
